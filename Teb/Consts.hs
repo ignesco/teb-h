7c211ac659910c3a4adb8b46aebb1d43e70c4aa6
CSH <chime@alyak>
2013-06-21 21:12:58 +0100

    Updated version number for next stable release with latest bug fixes

--- Teb/Consts.hs
+++ Teb/Consts.hs
@@ -1,6 +1,6 @@
 module Teb.Consts where
 
-version = "1.2.1"
+version = "1.2.2"
 versionDescription = "Teb repository manager (version " ++ version ++ ") (h)"
 coreDirectory = ".teb"
 manifestLocationFilename = "tebmanifestlocator.xml"
